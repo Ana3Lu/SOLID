cce5c72382a8e8d19da984bfa34d0c6808dc2b3d
juancego <140413383+juancego@users.noreply.github.com>
2024-08-01 10:46:17 -0500

    Eliminar getName ya implementado en NamedEmployee

--- Employee.java
+++ Employee.java
@@ -1,13 +1,9 @@
 package com.example.demo.unsolid;
 
-public class Employee {
+public abstract class Employee {
 
     String name;
     String departament;
-
-    public String getName() {
-        return name;
-    }
 
     public String getDepartament() {
         return departament;
